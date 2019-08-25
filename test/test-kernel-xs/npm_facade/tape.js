b1e9acfbcc1e8c594ae9ace5132c9f40591eab8d
Dan Connolly <dckc@madmode.com>
2019-08-24 23:25:27 -0500

    xs: oops! forgot to commit tape facade

---
+++ test/test-kernel-xs/npm_facade/tape.js
@@ -0,0 +1,81 @@
+// ack Paul Roub Aug 2014
+// https://stackoverflow.com/a/25456134/7963
+
+import { console } from './console';
+
+const { freeze } = Object;
+
+function deepEqual(x, y) {
+  if (x === y) {
+    return true;
+  }
+  if (
+    typeof x === 'object' &&
+    x != null &&
+    (typeof y === 'object' && y != null)
+  ) {
+    if (Object.keys(x).length !== Object.keys(y).length) {
+      return false;
+    }
+
+    for (const prop in x) {
+      // eslint-disable-next-line no-prototype-builtins
+      if (y.hasOwnProperty(prop)) {
+        if (!deepEqual(x[prop], y[prop])) {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+
+    return true;
+  }
+  return false;
+}
+
+export async function test(label, run) {
+  let result = null;
+
+  function fail(info) {
+    console.log(label, info);
+    result = false;
+  }
+
+  const t = freeze({
+    end() {
+      result = true;
+    },
+    equal(a, b) {
+      if (a !== b) {
+        fail('not equal. IOU details');
+      }
+    },
+    deepEqual: (a, b) => deepEqual(a, b),
+    throws(thunk, pattern) {
+      try {
+        thunk();
+        result = false;
+      } catch (ex) {
+        result = ex.message.match(pattern);
+      }
+    },
+    ok(a) {
+      result = !!a;
+    },
+    notOk(a) {
+      result = !a;
+    },
+  });
+
+  await run(t);
+
+  if (result === null) {
+    fail('not ended');
+  }
+  console.log(label, result ? 'PASS' : 'FAIL');
+}
+
+test.skip = function skip(label) {
+  console.log(label, 'SKIP');
+};
