32068e6a512f9ff1f3bd2d6168d001938908d00c
Dan Connolly <dckc@madmode.com>
2019-08-24 21:19:19 -0500

    xs: move test-queue-priority to its own file

--- test/test-kernel-xs/main.js
+++ test/test-kernel-xs/main.js
@@ -4,8 +4,7 @@ import { test } from 'tape';
 
 import makePromise from 'kernel/makePromise';
 
-// moddable timer
-import { setImmediate, setTimeout } from './moddableTimer';
+import testQueuePriority from './test-queue-priority';
 
 // import { makeCommsSlots } from './commsSlots/index';
 
@@ -21,25 +20,7 @@ export default function main() {
     t.end();
   });
 
-  test('Promise queue should be higher priority than IO/timer queue', async t => {
-    const log = [];
-    setImmediate(() => log.push(1));
-    setImmediate(() => {
-      log.push(2);
-      Promise.resolve().then(() => log.push(4));
-      log.push(3);
-    });
-    setImmediate(() => log.push(5));
-    setImmediate(() => log.push(6));
-
-    let r;
-    const p = new Promise(r0 => (r = r0));
-    setTimeout(() => r(), 0.1 * 1000);
-    await p;
-
-    t.deepEqual(log, [1, 2, 3, 4, 5, 6]);
-    return t.end();
-  });
+  testQueuePriority(test);
 
   /*
     test('build kernel', async t => {
