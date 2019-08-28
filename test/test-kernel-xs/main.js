c8a3d5d4ff420e6b50d1cd6a2ed996ca9236018f
Dan Connolly <dckc@madmode.com>
2019-08-28 01:02:47 -0500

    test-kernel-xs: build kernel test runs, fails

--- test/test-kernel-xs/main.js
+++ test/test-kernel-xs/main.js
@@ -1,15 +1,11 @@
 /* global trace */
 
 import { test } from 'tape';
-import { console } from 'console';
-
-// import makePromise from 'kernel/makePromise';
-// import buildKernel from 'kernel/index';
-
+import { setImmediate } from './moddableTimer';
 import testQueuePriority from './test-queue-priority';
 import testMarshal from './test-marshal';
 
-// import { makeCommsSlots } from './commsSlots/index';
+import buildKernel from 'kernel/index';
 
 export default function main() {
   trace('hi from test-kernel-xs with build task\n');
@@ -22,15 +18,14 @@ export default function main() {
   testQueuePriority(test);
   testMarshal(test);
 
-  /*
-    test('build kernel', async t => {
-      const kernel = buildKernel({ setImmediate });
-      await kernel.start(); // empty queue
-      const data = kernel.dump();
-      t.deepEqual(data.vatTables, []);
-      t.deepEqual(data.kernelTable, []);
-      t.end();
-    });
-    */
+  test('build kernel', async t => {
+    const kernel = buildKernel({ setImmediate });
+    await kernel.start(); // empty queue
+    const data = kernel.dump();
+    t.deepEqual(data.vatTables, []);
+    t.deepEqual(data.kernelTable, []);
+    t.end();
+  });
+
   trace('end of main\n');
 }
