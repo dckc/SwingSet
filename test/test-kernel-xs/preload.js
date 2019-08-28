e6ed771404482a2e4692042cc47daf637874846e
Dan Connolly <dckc@madmode.com>
2019-08-28 00:59:57 -0500

    xs preload: Nat too
    Lots of tests run now!

--- test/test-kernel-xs/preload.js
+++ test/test-kernel-xs/preload.js
@@ -1,8 +1,13 @@
+import Nat from '@agoric/nat';
 // Add makeHandeled to Promise; needed in test-marshal
 import maybeExtendPromise from '@agoric/eventual-send';
 import { console } from './console';
 
-globalThis.console = console; // used in @agoric/marshal
-globalThis.Promise = maybeExtendPromise(Promise);
+global.console = console; // used in @agoric/marshal
+global.Promise = maybeExtendPromise(Promise);
 
 Object.freeze(Promise);
+
+export function usesNat() {
+  Nat;
+}
